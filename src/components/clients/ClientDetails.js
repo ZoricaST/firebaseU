c374ab6368469631a1ad247366644036812541e0
Zorica Jelic <zoricajelicst@gmail.com>
2019-02-20 18:24:44 +0100

    nakon 62. lekcije

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -9,8 +9,60 @@ import Spinner from '../layout/Spinner';
 import classnames from 'classnames';
 
 class ClientDetails extends Component {
+    state = {
+        showBalanceUpdate: false,
+        balanceUpdateAmount: ''
+    }
+    //Update balance
+    balanceSubmit = e => {
+        e.preventDefault();
+        const { client, firestore } = this.props;
+        const { balanceUpdateAmount } = this.state;
+
+        const clientUpdate = {
+            balance: parseFloat(balanceUpdateAmount)
+        }
+        //update in firestore
+        firestore.update({ collection: 'clients', doc: client.id }, clientUpdate);
+
+    }
+    //Delete client
+    onDeleteClick = () => {
+        const { client, firestore, history } = this.props;
+        firestore.delete({ collection: 'clients', doc: client.id })
+            .then(history.push('/'));
+    }
+    onChange = e => this.setState({ [e.target.name]: e.target.value })
     render() {
         const { client } = this.props;
+        const { showBalanceUpdate, balanceUpdateAmount } = this.state;
+
+        let balanceForm = '';
+        // If balance form should display
+        if (showBalanceUpdate) {
+            balanceForm = (
+                <form onSubmit={this.balanceSubmit}>
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="balanceUpdateAmount"
+                            placeholder="Add New Balance"
+                            value={balanceUpdateAmount}
+                            onChange={this.onChange}
+                        />
+                        <div className="input-group-append">
+                            <input
+                                type="submit"
+                                value="Update"
+                                className="btn btn-outline-dark"
+                            />
+                        </div>
+                    </div>
+                </form>);
+        } else {
+
+        }
 
         if (client) {
             return (
@@ -25,11 +77,11 @@ class ClientDetails extends Component {
 
                     <div className="col-md-6">
                         <div className="btn-group float-right">
-                            <Link to={"/client/edit/${client.id}"} className="btn btn-dark">
+                            <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
                                 Edit
                         </Link>
 
-                            <button className="btn btn-danger">
+                            <button onClick={this.onDeleteClick} className="btn btn-danger">
                                 Delete
 </button>
 
@@ -54,8 +106,23 @@ class ClientDetails extends Component {
                                                 'text-danger': client.balance > 0,
                                                 'text-success': client.balance === 0
                                             })}>
-                                            ${parseFloat(client.balance).toFixed(2)}</span>
+                                            ${parseFloat(client.balance).toFixed(2)}
+                                        </span>
+                                        {''}
+                                        <small>
+                                            <a
+                                                href="#!"
+                                                onClick={() =>
+                                                    this.setState({
+                                                        showBalanceUpdate: !this.state.showBalanceUpdate
+                                                    })
+                                                }
+                                            >
+                                                <i className="fas fa-pencil-alt" />
+                                            </a>
+                                        </small>
                                     </h3>
+                                    {balanceForm}
                                 </div>
                             </div>
 
